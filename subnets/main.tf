90ade73c6d89dc5e4550981c43ae6777ed2a356f
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-06 23:33:56 +0530

    test

--- subnets/main.tf
+++ subnets/main.tf
@@ -18,8 +18,10 @@ resource "aws_route_table" "main" {
   }
 }
 
-resource "aws_route_table_association" "main" {
-  for_each       = var.subnets
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+output "subnet_id" {
+  value = aws_subnet.main
+}
+
+output "route_id" {
+  value = aws_route_table.main
 }

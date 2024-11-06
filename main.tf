42ba30b35d09ad3bb89fa5d9698c8087c15205fb
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-06 23:54:14 +0530

    test

--- main.tf
+++ main.tf
@@ -21,3 +21,10 @@ resource "aws_internet_gateway" "igw" {
     Name = "igw"
   }
 }
+
+# resource "aws_route" "r" {
+#   for_each                  = lookup(module.subnets, )
+#   route_table_id            =
+#   destination_cidr_block    = "10.0.1.0/22"
+#   vpc_peering_connection_id = "pcx-45ff3dc1"
+# }

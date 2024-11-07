414e9d09dce9b3a2d88fd498a4fe6b3e59278bb5
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-07 14:10:43 +0530

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 
@@ -64,4 +72,42 @@ resource "aws_route" "default-peer-entry" {
   route_table_id            = var.default_route_table_id
   destination_cidr_block    = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_instance" "main" {
+  instance_type  = "t2.micro"
+  ami            = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id     = local.app_subnet_ids[0]
+
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+
 }

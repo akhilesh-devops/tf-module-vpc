210ccf7b1e29f35b30e25440c3f226b489c3e8c3
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-08 16:08:05 +0530

    test

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ module "subnets" {
 
 }
 
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = var.vpc_id
-#
-#   tags = {
-#     Name = "igw"
-#   }
-# }
-#
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
 # resource "aws_route" "route" {
 #   for_each                  = var.subnets
 #   route_table_id            = lookup(lookup(aws_route_table.rt, each.key, null), "id", null)

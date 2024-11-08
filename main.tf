219e9c3a60049457f1827988614697ff12a35698
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-08 16:34:40 +0530

    test

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# resource "aws_route" "route" {
-#   for_each                  = var.subnets
-#   route_table_id            = lookup(lookup(module., each.key, null), "id", null)
-#   destination_cidr_block    = "0.0.0.0/0"
-#   gateway_id                = aws_internet_gateway.igw.id
-# }
+resource "aws_route" "igw" {
+  for_each                  = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id            = each.key["id"]
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.igw.id
+}
 

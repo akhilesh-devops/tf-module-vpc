1517ba6b0ca9ee31ed8c3834926880b7bad178d6
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-14 11:42:49 +0530

    test

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 # locals {
 #   #subnets
-#   public_subnet_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "id", null): v.id]
-#   app_subnet_ids    = [for k,v in lookup(lookup(module.subnets, "app", null), "id", null): v.id]
+#   public_subnet_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "subnets_ids", null): v.id]
+#   app_subnet_ids    = [for k,v in lookup(lookup(module.subnets, "app", null), "subnets_ids", null): v.id]
 #   db_subnet_ids     = [for k,v in lookup(lookup(module.subnets, "db", null), "id", null): v.id]
 #
 #   #Routetable

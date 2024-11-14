f6d65124f8bfe5fcab1de13a48b06363a7a4963f
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-14 11:57:54 +0530

    test

--- locals.tf
+++ locals.tf
@@ -6,8 +6,8 @@ locals {
   private_subnet_ids = concat(local.app_subnet_ids, local.db_subnet_ids)
 
   #Routetable
-  public_route_table_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "id", null): v.id]
-  app_route_table_ids    = [for k,v in lookup(lookup(module.subnets, "app", null), "id", null): v.id]
-  db_route_table_ids     = [for k,v in lookup(lookup(module.subnets, "db", null), "id", null): v.id]
+  public_route_table_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "route_table_ids", null): v.id]
+  app_route_table_ids    = [for k,v in lookup(lookup(module.subnets, "app", null), "route_table_ids", null): v.id]
+  db_route_table_ids     = [for k,v in lookup(lookup(module.subnets, "db", null), "route_table_ids", null): v.id]
   private_route_table_ids = concat(local.app_route_table_ids, local.db_route_table_ids)
 }

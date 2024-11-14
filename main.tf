60aece336fbc10e9754b9d5feb0a542cbb92c120
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-14 10:57:06 +0530

    test

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "main" {
     Name = "dev"
   }
 }
+
+module "subnets" {
+  source = "./module"
+
+  for_each = var.subnets
+  subnets  = each.value
+  vpc      = aws_vpc.main.id
+
+}

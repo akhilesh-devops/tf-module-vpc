7f0a4e0919186e64135677ca305f63f6c759de51
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-18 12:10:59 +0530

    test

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   #subnets
-  public_subnet_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "subnet_ids", null)]
-  app_subnet_ids    = [ for k,v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null)]
-  db_subnet_ids     = [ for k,v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null)]
+  public_subnet_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "subnet_ids", null): v.id]
+  app_subnet_ids    = [for k,v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null): v.id]
+  db_subnet_ids     = [ for k,v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null): v.id]
   private_subnet_ids = concat(local.app_subnet_ids, local.db_subnet_ids)
 }

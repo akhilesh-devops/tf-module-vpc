f5c42ad94515ab35465e126d82202bbacb5c5abb
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-18 12:14:25 +0530

    test

--- main.tf
+++ main.tf
@@ -42,7 +42,5 @@ resource "aws_nat_gateway" "ngw" {
   allocation_id = element(aws_eip.ngw.*.id, count.index)
   subnet_id     = element(local.public_subnet_ids, count.index)
 
-  tags = {
-    Name = "ngw-${count.index+1}"
-  }
+
 }

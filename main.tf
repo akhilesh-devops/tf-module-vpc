d40a78398db01a685b044b8fb5b0e46edb1b1290
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-18 11:50:29 +0530

    test

--- main.tf
+++ main.tf
@@ -27,3 +27,9 @@ resource "aws_route" "rt" {
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.igw.id
 }
+
+resource "aws_eip" "eip" {
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+
+  domain   = "vpc"
+}

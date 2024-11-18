c40682fd8e18a80473eddd8c47c97521c55098b8
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-18 12:00:41 +0530

    test

--- main.tf
+++ main.tf
@@ -28,10 +28,20 @@ resource "aws_route" "rt" {
   gateway_id                = aws_internet_gateway.igw.id
 }
 
-resource "aws_eip" "eip" {
+resource "aws_eip" "ngw" {
   for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
 
   domain   = "vpc"
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  for_each      = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  allocation_id = lookup(aws_eip.ngw, "*.id", null)
+  subnet_id     = each.value["id"]
+
   tags = {
     Name = each.key
   }
